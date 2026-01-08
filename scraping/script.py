53172867f891d0248601a094bda61cde4b052019
Toni Martin <toni-mr-1996@hotmail.com>
2026-01-08 15:37:45 +0100

    Update script.py

--- scraping/script.py
+++ scraping/script.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from fastapi import HTTPException
 from config.redisClient import redisClient
 from redis.exceptions import RedisError
-
+from typing import List
 
 import json
 import re
@@ -24,12 +24,14 @@ def fetch_mawaqit(masjid_id:str):
         if retrieved_data:
             return json.loads(retrieved_data)
 
-    r = requests.get(f"https://mawaqit.net/fr/{masjid_id}")
+    url = f"https://mawaqit.net/fr/m/{masjid_id}"
+    r = requests.get(url)
     if r.status_code == 200:
+        soup = BeautifulSoup(r.text, 'html.parser')
         searchString = r'(?:var|let)\s+confData\s*=\s*(.*?);'
         script = soup.find('script', string=re.compile(searchString, re.DOTALL))
         if script:
-            mawaqit = re.search(r'var confData = (.*?);', script.string, re.DOTALL)
+            mawaqit = re.search(searchString, script.string, re.DOTALL)
             if mawaqit:
                 conf_data_json = mawaqit.group(1)
                 conf_data = json.loads(conf_data_json)
@@ -43,14 +45,16 @@ def fetch_mawaqit(masjid_id:str):
             print("Script containing confData not found.")
             raise HTTPException(status_code=500, detail=f"Script containing confData not found for {masjid_id}")
     if r.status_code == 404:
-        raise HTTPException(status_code=404, detail=f"{masjid_id} not found") 
+        raise HTTPException(status_code=404, detail=f"{masjid_id} not found")  
+    if r.status_code == 500:
+        raise HTTPException(status_code=502, detail=f"something went wrong fetching {url}") 
 
 def get_prayer_times_of_the_day(masjid_id):
     confData = fetch_mawaqit(masjid_id)
     times = confData["times"]
-    sunset = confData["shuruq"]
-    prayer_time = models.PrayerTimes(fajr=times[0], sunset=sunset, dohr=times[1], asr=times[2], maghreb=times[3], icha=times[4])
-    prayer_dict = prayer_time.dict()
+    sunrise = confData["shuruq"]
+    prayer_time = models.PrayerTimes(fajr=times[0], sunrise=sunrise, dohr=times[1], asr=times[2], maghreb=times[3], icha=times[4])
+    prayer_dict = prayer_time.model_dump()
     return prayer_dict
 
 def get_calendar(masjid_id):
@@ -65,7 +69,7 @@ def get_month(masjid_id, month_number):
     prayer_times_list = [
         models.PrayerTimes( 
             fajr=prayer[0],
-            sunset=prayer[1],
+            sunrise=prayer[1],
             dohr=prayer[2],
             asr=prayer[3],
             maghreb=prayer[4],
@@ -74,3 +78,31 @@ def get_month(masjid_id, month_number):
         for prayer in month.values()
     ]
     return prayer_times_list
+
+def get_month_iqama(masjid_id, month_number):
+    if month_number < 1 or month_number > 12:
+        raise HTTPException(status_code=400, detail=f"Month number should be between 1 and 12")
+    confData = fetch_mawaqit(masjid_id)
+    month = confData["iqamaCalendar"][month_number - 1]
+    iqama_times_list = [
+        models.IqamaPrayerTimes( 
+            fajr=iqama[0],
+            dohr=iqama[1],
+            asr=iqama[2],
+            maghreb=iqama[3],
+            icha=iqama[4]
+        )
+        for iqama in month.values()
+    ]
+
+    return iqama_times_list
+
+def get_announcements(masjid_id: int) -> List[models.Announcement]:
+    confData = fetch_mawaqit(masjid_id)
+    announcements = confData.get("announcements", [])
+    return [models.Announcement(**a) for a in announcements]
+
+def get_services(masjid_id: int) -> models.MosqueServices:
+    confData = fetch_mawaqit(masjid_id)
+    mosque_services = models.MosqueServices(**confData)
+    return mosque_services
